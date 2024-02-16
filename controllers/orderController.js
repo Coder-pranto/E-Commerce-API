ff26305d36056c20b9a575845b1bc90b9ce4b045
Riaj Hasan Pranto <54685875+Coder-pranto@users.noreply.github.com>
2024-02-17 00:16:53 +0600

    updated: order functionality completed(fakeapi)

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -66,19 +66,44 @@ const createOrder = async (req, res) => {
 };
 
 
-
-  const getAllOrders = async (req, res) => {
-    
+const getAllOrders = async (req, res) => {
+    const orders = await Order.find({});
+    res.status(StatusCodes.OK).json({ orders, count: orders.length });
   };
+
+
+  const getSingleOrder = async (req, res) => {
+    const { id: orderId } = req.params;
+    const order = await Order.findOne({ _id: orderId });
+    if (!order) {
+      throw new CustomError.NotFoundError(`No order with id : ${orderId}`);
+    }
+    checkPermissions(req.user, order.user);
+    res.status(StatusCodes.OK).json({ order });
+  };
+
 
   const getCurrentUserOrders = async (req, res) => {
-  res.send("curr")
+    const orders = await Order.find({ user: req.user.userId });
+    res.status(StatusCodes.OK).json({ orders, count: orders.length });
   };
-  const getSingleOrder = async (req, res) => {
 
-  };
+  
   const updateOrder = async (req, res) => {
-   res.send("update")
+    const { id: orderId } = req.params;
+    const { paymentIntentId } = req.body;
+  
+    const order = await Order.findOne({ _id: orderId });
+    if (!order) {
+      throw new CustomError.NotFoundError(`No order with id : ${orderId}`);
+    }
+    checkPermissions(req.user, order.user);
+  
+    order.paymentIntentId = paymentIntentId;
+    order.status = 'paid';
+    await order.save();
+  
+    res.status(StatusCodes.OK).json({ order });
   };
   
   module.exports = {
